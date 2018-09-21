0d47004f235b8e920a0f59d0ee3297c2a038c055
Ochsner, David <david.ochsner@empa.ch>
2018-09-21 10:45:00 +0000

    Working on verify_chain"

--- jobs/verify_chain.py
+++ jobs/verify_chain.py
@@ -6,9 +6,22 @@
 
 # Author: dao, david.ochsner@empa.ch
 
+import os
 import netCDF4 as nc
 
+def import_to_netCDF4(filename, mode='r'):
+    return nc.Dataset(filename, mode)
+
 def main(start_time, hstart, hstop, cfg):
+    reference_file_name = "reference_lffd2015010200.nc"
     print("Verification!")
     print("Looking at: " + str(cfg.reference_dir))
+
+    reference_file_path = os.path.join(cfg.reference_dir, reference_file_name)
+
+    ref_data = import_to_netCDF4(reference_file_path)
+
+    print(ref_data['T'][0,10,10,10])
+
+
 
