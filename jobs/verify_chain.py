523a6ffd2c736eb6e171c712246cdd2146446eac
Ochsner, David <david.ochsner@empa.ch>
2018-09-21 08:55:47 +0000

    Working on verify-job

---
+++ jobs/verify_chain.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Job to verify the correctness of the chain. The output of the example-case is
+# compared to a reference output.
+
+# Author: dao, david.ochsner@empa.ch
+
+import os
+import logging
+import netCDF4 as nc
+from numpy import allclose
+
+def import_data(filename, mode='r'):
+    return nc.Dataset(filename, mode)
+
+
+def import_datasets(ref_path, run_path):
+    """
+    Read the reference and run datasets
+    """
+    try:
+        ref_data = import_data(ref_path)
+    except:
+        logging.error("Reading reference data failed")
+        raise
+    try:
+        run_data = import_data(run_path)
+    except:
+        logging.error("Reading run data failed")
+        raise
+    return ref_data, run_data
+
+
+def compare_vals(dataset1, dataset2, variables):
+    for var in variables:
+        # only for easy debugging
+        print("Comparing " + var)
+        print(dataset1[var][0,0,0,0])
+        print(dataset2[var][0,0,0,0])
+        
+        # what should happen on error:
+        #   -raise exception -> mail to user, incomplete test
+        #   -logging.error -> i think it prints to out and continues
+        #   -finish comparing, just write to logfile -> how to notify user?
+        if not allclose(dataset1[var], dataset2[var]):
+            logging.error("cosmo-ouput is not equal for " + var)
+            break
+        logging.info(var + ": Passed")
+
+
+def main(starttime, hstart, hstop, cfg):
+    logging.info("Started verification")
+    for (ref_file, run_file), variables in cfg.values_to_check.items():
+        logging.info("Comparing " + str(variables))
+
+        # reference file location
+        ref_file_path = os.path.join(cfg.reference_dir, ref_file)
+
+        # run data location
+        if cfg.output_dir is None:
+            # Standard output location
+            run_file_path = os.path.join(cfg.output_root,
+                                         starttime.strftime('%Y%m%d%H') +
+                                         "_" + str(int(hstart)) + "_" +
+                                         str(int(hstop)),
+                                         "cosmo_output",
+                                         run_file)
+        else:
+            # User-provided output location
+            run_file_path = os.path.join(cfg.output_dir, run_file)
+
+        logging.info("Output file: " + str(run_file_path))
+        logging.info("Reference file: " + str(ref_file_path))
+
+        # read data
+        ref_data, run_data = import_datasets(ref_file_path, run_file_path)
+
+        #compare data
+        compare_vals(ref_data, run_data, variables)
+
+    logging.info("Finished verification")
