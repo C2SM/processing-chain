b54bb814395c54533458bf3d57cd3a70a6937222
Michael Jähn <michael.jaehn@empa.ch>
2018-11-05 15:16:27 +0100

    Add nudging case

--- cases/jae-cs-nudging/config.py
+++ cases/jae-cs-nudging/config.py
@@ -4,88 +4,74 @@ import types
 
 user = os.environ['USER']
 mail_address = {
-    'gkuhl':     'gerrit.kuhlmann@empa.ch',
-    'kug':       'gerrit.kuhlmann@empa.ch',
-    'dbrunner':  'dominik.brunner@empa.ch',
-    'haussaij':  'jean-matthieu.haussaire@empa.ch',
-    'mjaehn':    'michael.jaehn@empa.ch',
-    'muq':       'qing.mu@empa.ch',
-    'parsenov' : 'pavle.arsenovic@empa.ch',
-    'ochsnerd' : 'david.ochsner@empa.ch',
+    'gkuhl':    'gerrit.kuhlmann@empa.ch',
+    'kug':      'gerrit.kuhlmann@empa.ch',
+    'dbrunner': 'dominik.brunner@empa.ch',
+    'haussaij': 'jean-matthieu.haussaire@empa.ch',
+    'mjaehn':   'michael.jaehn@empa.ch',
 }[user]
-
 
 # Everything defined before is not written to os.environ
 # and thus not available to bash scripts.
 not_config = list(locals().keys())
 
-
 compute_host = 'daint'
-compute_queue = 'debug' #'debug' #'normal'
-compute_account = 'em05' #'pr04'
+compute_queue = 'normal' #'debug' #'normal'
+compute_account = 'sd02' #'pr04'
 
 # case name = pathname in cases/
 path = os.path.realpath(__file__)
-casename = os.path.basename(os.path.dirname(path)) 
+casename = os.path.basename(os.path.dirname(path))
 
 # input root
-input_root = '/store/empa/em05/input_processing_chain_example/'
+input_root = '/store/empa/em05/mjaehn/Carbosense/input'
 meteo_dir = os.path.join(input_root, 'meteo')
 meteo_prefix = "laf"
 meteo_inc = 1
 
 # output
-output_root = os.environ['SCRATCH'] + "/processing_chain/output/" + casename
+output_root = '/scratch/snx3000/mjaehn/Carbosense/output/%s' % casename
 
 # working root
-work_root = os.environ['SCRATCH'] + "/processing_chain"
+work_root = '/scratch/snx3000/mjaehn/Carbosense'
 log_dir = os.path.join(work_root, 'logs')
 
-
 # anthropogenic emissions pre-processed for mother and nested domain
-emissions_dir = os.path.join(input_root, 'emissions_coarse')
-emis_gridname = "CO2_CO_NOX_Berlin-coarse_"
-
+emissions_dir = os.path.join(input_root, 'emissions')
+emis_gridname = 'CO2_CO_CH4_Carbosense-1.0_'
 
 # VPRM biogenic fluxes
-vprm_dir = os.path.join(input_root,'vprm_smartcarb','processed')
-vprm_prefix = ["vprm_"] #could be ["gpp_", "ra_"]
+vprm_dir = os.path.join(input_root,'vprm','processed')
+vprm_prefix = ['gpp_', 'ra_'] 
 
 # CAMS for CO2, CO and NOX initial and boundary conditions
-
-# if the data is already preprocessed and just need to be copied, 
-# - cams_dir_orig is not used
-# - cams_dir_proc is where your data to be copied is
-# - cams_parameters should have one element per type of file you need to copy. It should have:
-#       - "suffix" : files are called cams_dir_proc/suffix_date.nc
-#       - "inc" : increment between icbc data 
 cams_dir_orig = os.path.join(input_root, 'icbc') #Input directory
 cams_dir_proc = os.path.join(input_root, 'icbc', 'processed') #Output directory
-
-
-# If the data is not yet preprocessed and needs to run cams4int2cosmo
-# cams_parameters should have one element per type of file you need to output. It should have:
+# required parameters for cams preprocessing
+# The list should contain one element per output file
+# If cams4int2cosmo.py is used, need all the following, otherwise just the suffix
 # - species : the list of species to put in said file (within CO2, CO, CH4, NOX)
 # - inc : the increment between timesteps
 # - prefix1 : the input file prefix (cams_dir_orig/prefix1_date.nc)
 # - prefix2 : the input surface pressure file prefix (cams_dir_orig/prefix2_date.nc)
 # - lev : the number of levels (137 or 60)
 # - suffix : for the output file (cams_dir_proc/suffix_date.nc)
-cams_parameters = [{
-    "suffix" : "cams_co2",
-    "inc" : 3
-     # "species" :["CO2","CO","CH4"],     
-     # "prefix1":"cams_gf39",
-     # "prefix2":"sfc_gf39",
-     # "lev":137,
-},
-    {"suffix":"cams_nox",
-     "inc" : 3,
-     # "species" :["NOX"],     
-     # "prefix1":"cams_0001",
-     # "prefix2":"sfc_0001",
-     # "lev":60,
-     }]
+cams_parameters = [
+    {'suffix':'cams_co2',
+     # ' species' :['CO2','CO','CH4'],
+     'inc' : 3,
+     # 'prefix1':'cams_gf39',
+     # 'prefix2':'sfc_gf39',
+     # 'lev':137,
+     }
+     #{'suffix':'cams_nox',
+     # 'species' :['NOX'],
+     # 'inc' : 3,
+     # 'prefix1':'cams_0001',
+     # 'prefix2':'sfc_0001',
+     # 'lev':60,
+     #}
+    ]
 
 # CarbonTracker for CO2, CO and NOX initial and boundary conditions
 # ct_dir_orig = os.path.join(input_root, 'icbc') #Input directory
@@ -100,40 +86,37 @@ cams_parameters = [{
 # # - suffix : for the output file (cams_dir_proc/suffix_YYYYMMDDHH.nc)
 # # as of today, it only computes CO2
 # ct_parameters = [
-#     {"inc" : 24,
-#     "prefix":"CT2016.molefrac_glb3x2",
-#     "suffix":"ct"}]
+#     {'inc' : 24,
+#     'prefix':'CT2016.molefrac_glb3x2',
+#     'suffix':'ct'}]
 
 
 
-# chain root (TODO: remove)
+# chain root
 chain_src_dir = os.getcwd()
 tools_dir = os.path.join(chain_src_dir, 'jobs/tools')
 
 # some constants for scripts
 del_tmp_timeout = 120   # timeout in days after which temporary files 
                         # like restarts (last forecast hour of day - 1) are removed
-                        # by "purgetmp"
+                        # by 'purgetmp'
 
 meteo_spinup = 0        # time in hours the model is integrated before transport simulation 
                         # is started (i.e. spinupstart = inidate - meteospinup)
                         # NOTE: has to be multiple of meteo increment (e.g 3hrs for IFS)
 
-
 # INT2LM
 int2lm_extpar_dir = os.path.join(input_root, 'extpar')
-int2lm_extpar_file = "test_domain.nc"
-int2lm_bin = os.path.join(input_root,"executables/int2lm") 
+int2lm_extpar_file = 'extpar_empa_cosmo1_aster_opt.nc'
+int2lm_bin = '/users/mjaehn/github/int2lm/int2lm_tracer_conversion_cray'
 
-#post_int2lm
-post_int2lm_species = ["CO2_BG"]#,"CO_BG","CH4_BG","NOX_BG"]
-
+# post_int2lm
+post_int2lm_species = ['CO2_BG', 'CO_BG', 'CH4_BG']
 
 # COSMO
-cosmo_bin=  os.path.join(input_root,"executables/cosmo") 
+cosmo_bin = '/users/mjaehn/github/cosmo-pompa/cosmo/cosmo_smartcarb_ser_gpu'
 
 # Case specific settings (int2lm and cosmo namelists and runscripts)
-
 int2lm_namelist = '%s/cases/%s/int2lm_INPUT.cfg' % (chain_src_dir,casename)
 int2lm_runjob = '%s/cases/%s/int2lm_runjob.cfg' % (chain_src_dir,casename)
 cosmo_namelist = '%s/cases/%s/cosmo_INPUT_' % (chain_src_dir,casename)
@@ -144,7 +127,7 @@ cosmo_runjob = '%s/cases/%s/cosmo_runjob.cfg' % (chain_src_dir,casename)
 
 ## INT2LM
 if compute_queue=="normal":
-    int2lm_walltime="01:00:00"
+    int2lm_walltime="12:00:00"
 elif compute_queue=="debug":
     int2lm_walltime="00:30:00"
 else: 
@@ -159,7 +142,7 @@ int2lm_np_tot = int2lm_np_x * int2lm_np_y
 
 ## COSMO 
 if compute_queue=="normal":
-    cosmo_walltime="01:00:00"
+    cosmo_walltime="18:00:00"
     cosmo_np_x=6
     cosmo_np_y=5
 elif compute_queue=="debug":
@@ -170,25 +153,8 @@ else:
     logging.error("Unknown queue name: %s" % compute_queue)
     sys.exit(1)
 
-cosmo_np_io = 0
+cosmo_np_io = 1
 cosmo_np_tot = cosmo_np_x * cosmo_np_y + cosmo_np_io     
 
 # Restart 
-restart_step = 24
-
-## Verification
-reference_dir = os.path.join(input_root, "reference_output")
-
-# If the output file that gets compared to the reference is not at the location
-# that post_cosmo copied it to, give the path to it here. Else leave it 'None'
-output_dir = None
-# Use this if the post_cosmo job is not executed
-# output_dir = os.environ['SCRATCH'] + "/processing_chain/example/2015010100_0_24/cosmo/output"
-
-# variables_to_check is a dict() with a tuple() of filenames as key and a list
-# of variables-names as value. The tuple consists of the filenames of the two
-# files to check, the list contains the variable-names that are compared.
-# The verify_chain job will look for the files in the reference_dir (first tuple
-# element) and the ouput_dir (second tuple element)
-values_to_check = {("reference_lffd2015010200.nc","lffd2015010200.nc") :
-                      ['T', 'U', 'V', 'CO2_A']}
+restart_step = 240
