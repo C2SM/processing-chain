45698c0663e3ddb3d4f8e1568ecdf3c35720e7ee
dao <david.ochsner@empa.ch>
2018-11-07 11:04:56 +0100

    Obs_nudging for cosmo, ignore missing data (#6)

--- jobs/obs_nudging.py
+++ jobs/obs_nudging.py
@@ -11,13 +11,13 @@ from . import tools
 def main(starttime,hstart,hstop,cfg):
     """Copy and rename the obs_nudging files to the **COSMO** input directory.
 
-    Only necessary for **COSMOART** simulations.
-
     In the folder ``cfg.obs_nudging_dir``, the files are saved in the format
     ``{prefix}-YYYYMMDD000000-YYYYMMDD000000``. **COSMO** expects files in the 
     format ``{prefix}x``, where ```x`` is ``[nothing], .2, .3, .4, ...``. This 
     job handles this filename-change and copies them to the **COSMO** input 
     folder on scratch (``cfg.cosmo_input/obs_nudging``).
+
+    Missing observation files are ignored.
 
     Also copies the blacklist-file blklsttmp used by the nested run.
 
@@ -32,8 +32,6 @@ def main(starttime,hstart,hstop,cfg):
     cfg : config-object
         Object holding all user-configuration parameters as attributes
     """
-    tools.check_target(cfg, 'cosmoart')
-
     dest_dir = os.path.join(cfg.cosmo_input,"obs_nudging")
     tools.create_dir(dest_dir, "obs nudging input")
 
@@ -41,30 +39,27 @@ def main(starttime,hstart,hstop,cfg):
                  .format(cfg.obs_nudging_dir,
                          dest_dir))
 
-    count = 0  # running index for COSMOART
-    for time in tools.iter_hours(starttime, hstart, hstop, step=24):
-        count += 1
+    for i, t in enumerate(tools.iter_hours(starttime, hstart, hstop, step=24)):
         for prefix in cfg.obs_nudging_prefixes:
             src_filename = (prefix +
-                            time.strftime(cfg.obs_nudging_date_format) +
-                            (time + timedelta(days=1))
+                            t.strftime(cfg.obs_nudging_date_format) +
+                            (t + timedelta(days=1))
                             .strftime(cfg.obs_nudging_date_format)
                            )
             src_path = os.path.join(cfg.obs_nudging_dir, src_filename)
-            if count == 1:
+            if i == 1:
                 dest_path = os.path.join(dest_dir, prefix)
             else:
-                dest_path = os.path.join(dest_dir, "{}.{}".format(prefix, count))
+                dest_path = os.path.join(dest_dir, "{}.{}".format(prefix, i))
 
             try:
                 shutil.copy(src_path, dest_path)
             except FileNotFoundError:
-                logging.error("Emission input file not found at {}, or output"
-                              " directory doesn't exist to copy {}"
-                              .format(src_file, dest_path))
-                raise
+                # just ignore missing nudging-data
+                logging.info("No nudging data at {}".format(src_filename))
+                continue
             except (PermissionError, OSError):
-                logging.error("Copying emission data file failed")
+                logging.error("Copying nudging data file failed")
                 raise
             logging.info("Copied file {} to {}".format(src_filename, dest_path))
 
