962004688b461dd9ec1314bd240696e6d2528eb6
Michael Jähn <mjaehn@ethz.ch>
2022-08-11 14:22:58 +0200

    Install cosmo-ghg in own spack instance

---
+++ jenkins/scripts/install_cosmo-ghg.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e -x
+
+function error {
+    echo "*** Error: $@" >&2
+    exit 1
+}
+
+# Check if script is called correctly
+[[ $(git rev-parse --show-toplevel 2>/dev/null) = $(pwd) ]] || error "$0 not launched from toplevel of repository"
+
+SPACK_BRANCH='master'
+GIT_REMOTE='https://github.com/C2SM/spack-c2sm.git'
+
+SPACK_SPEC=$(cat cases/cosmo-ghg-11km-test/cosmo_spec)
+
+git clone -b ${SPACK_BRANCH} ${GIT_REMOTE}
+
+cd spack-c2sm
+# install spack temp instance with branch config files and mch spack packages
+./config.py -m daint -i . -r ./spack/etc/spack -p $PWD/spack -s $PWD/spack -u ON -c ./spack-cache
+# source spack temp instance
+. spack/share/spack/setup-env.sh
+cd ..
+spack installcosmo -v ${SPACK_SPEC}
