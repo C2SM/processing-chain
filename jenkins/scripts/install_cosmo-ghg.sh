cb56b2001701c1c48257f25b1d270aa4b2bf72fd
Michael Jähn <mjaehn@ethz.ch>
2022-08-11 14:36:46 +0200

    Use correct spack instance

--- jenkins/scripts/install_cosmo-ghg.sh
+++ jenkins/scripts/install_cosmo-ghg.sh
@@ -17,10 +17,11 @@ SPACK_SPEC=$(cat cases/cosmo-ghg-11km-test/cosmo_spec)
 
 git clone -b ${SPACK_BRANCH} ${GIT_REMOTE}
 
-cd spack-c2sm
+pushd spack-c2sm
 # install spack temp instance with branch config files and mch spack packages
 ./config.py -m daint -i . -r ./spack/etc/spack -p $PWD/spack -s $PWD/spack -u ON -c ./spack-cache
 # source spack temp instance
 . spack/share/spack/setup-env.sh
-cd ..
+popd
+
 spack installcosmo -v ${SPACK_SPEC}
