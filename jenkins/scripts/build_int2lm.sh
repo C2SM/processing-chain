7ed2b2e66cf70e1a83f046c33295da823b6b9d9e
Michael Jähn <mjaehn@ethz.ch>
2023-05-17 14:28:59 +0200

    devbuilds for int2lm and cosmo

---
+++ jenkins/scripts/build_int2lm.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e -x
+
+function error {
+    echo "*** Error: $@" >&2
+    exit 1
+}
+
+# Check if script is called correctly
+[[ $(git rev-parse --show-toplevel 2>/dev/null) = $(pwd) ]] || error "$0 not launched from toplevel of repository"
+
+BRANCH=c2sm-features
+GIT_REMOTE=git@github.com:C2SM-RCM/int2lm.git
+
+pushd src
+# Activate spack
+. spack-c2sm/setup-env.sh
+
+# Remove cosmo-ghg folder (if existing)
+rm -fr int2lm
+
+# Clone cosmo-ghg
+git clone -b ${BRANCH} ${GIT_REMOTE}
+    pushd int2lm
+    spack dev-build int2lm @dev-build %nvhpc
+    popd
+popd
