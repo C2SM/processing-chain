2f656dcd1f4fedccb25250d0f6bc0d227b4d0414
Michael Jähn <mjaehn@ethz.ch>
2023-06-26 14:34:58 +0200

    Adapt data script due to new input data structure

--- jenkins/scripts/get_data.sh
+++ jenkins/scripts/get_data.sh
@@ -10,6 +10,9 @@ function error {
 # Check if script is called correctly
 [[ $(git rev-parse --show-toplevel 2>/dev/null) = $(pwd) ]] || error "$0 not launched from toplevel of repository"
 
+mkdir -p input
+cd input
 wget ftp://iacftp.ethz.ch/pub_read/mjaehn/input_processing-chain/input_processing-chain.tgz 
 tar -xvzf input_processing-chain.tgz
 rm -f input_processing-chain.tgz
+cd ..
