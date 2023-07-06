72fb10a6145f0b44cb30622d951b280e1e812f0b
Michael Jähn <mjaehn@ethz.ch>
2023-07-06 09:36:40 +0200

    Add actual test scripts

---
+++ jenkins/scripts/test_cosmo-ghg.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e -x
+
+function error {
+    echo "*** Error: $@" >&2
+    exit 1
+}
+
+# Check if script is called correctly
+[[ $(git rev-parse --show-toplevel 2>/dev/null) = $(pwd) ]] || error "$0 not launched from toplevel of repository"
+
+python run_chain.py cosmo-ghg-11km-test 2015-01-01 0 24 -f
---
+++ jenkins/scripts/test_icon-art.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e -x
+
+function error {
+    echo "*** Error: $@" >&2
+    exit 1
+}
+
+# Check if script is called correctly
+[[ $(git rev-parse --show-toplevel 2>/dev/null) = $(pwd) ]] || error "$0 not launched from toplevel of repository"
+
+python run_chain.py icon-art-test 2018-01-01 0 24 -j prepare_data icon -f
