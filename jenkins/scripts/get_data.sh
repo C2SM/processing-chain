598c04154953a32f3c4750c490f1b5b772b0d204
Michael Jähn <mjaehn@ethz.ch>
2023-07-12 14:16:16 +0200

    Change input data path

--- jenkins/scripts/get_data.sh
+++ jenkins/scripts/get_data.sh
@@ -12,7 +12,7 @@ function error {
 
 mkdir -p input
 pushd input
-    wget ftp://iacftp.ethz.ch/pub_read/mjaehn/input_processing-chain/input_processing-chain.tgz 
+    wget ftp://iacftp.ethz.ch/pub_read/mjaehn/input_processing-chain.tgz 
     tar -xvzf input_processing-chain.tgz
     rm -f input_processing-chain.tgz
 popd
