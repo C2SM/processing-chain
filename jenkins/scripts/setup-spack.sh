48320711797cfdbee386349075a2e9c2a04d54ba
Michael Jähn <mjaehn@ethz.ch>
2023-09-18 09:43:08 +0200

    Use fixed spack tag

--- jenkins/scripts/setup-spack.sh
+++ jenkins/scripts/setup-spack.sh
@@ -10,13 +10,13 @@ function error {
 # Check if script is called correctly
 [[ $(git rev-parse --show-toplevel 2>/dev/null) = $(pwd) ]] || error "$0 not launched from toplevel of repository"
 
-BRANCH=main
+TAG=v0.18.1.12
 GIT_REMOTE=https://github.com/C2SM/spack-c2sm.git
 
 rm -fr src/spack-c2sm
 
 pushd src
-git clone --depth 1 --recurse-submodules --shallow-submodules -b ${BRANCH} ${GIT_REMOTE}
+git clone --depth 1 --recurse-submodules --shallow-submodules -b ${TAG} ${GIT_REMOTE}
 . spack-c2sm/setup-env.sh
 popd
 
