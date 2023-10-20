f3039e6c5784c9fbee8701762ec53e93f6bfbd14
Michael Jähn <mjaehn@ethz.ch>
2023-10-20 08:51:57 +0200

    Further work on icon job

--- jobs/icon.py
+++ jobs/icon.py
@@ -14,7 +14,7 @@
 import logging
 import os
 import subprocess
-from . import tools
+from . import tools, prepare_data
 from datetime import timedelta
 
 
@@ -29,7 +29,7 @@ def main(starttime, hstart, hstop, cfg, model_cfg):
     and ``cfg.icon_restart_out``).
 
     Copy the **ICON**-executable from
-    ``cfg.icon_bin`` to ``cfg.icon_work/icon.exe``.
+    ``cfg.icon_binary_file`` to ``cfg.icon_work/icon.exe``.
 
     Use the tracer-csv-file to append **ICON**-namelist file.
 
@@ -50,6 +50,8 @@ def main(starttime, hstart, hstop, cfg, model_cfg):
     cfg : config-object
         Object holding all user-configuration parameters as attributes
     """
+    cfg = prepare_data.set_cfg_variables(cfg, starttime, hstart, hstop)
+
     logfile = os.path.join(cfg.log_working_dir, "icon")
     logfile_finish = os.path.join(cfg.log_finished_dir, "icon")
 
@@ -57,8 +59,9 @@ def main(starttime, hstart, hstop, cfg, model_cfg):
                  "submit the job to the queue")
 
     # Copy icon executable
+    cfg.print_config()
     execname = 'icon.exe'
-    tools.copy_file(cfg.icon_bin, os.path.join(cfg.icon_work, execname))
+    tools.copy_file(cfg.icon_binary_file, os.path.join(cfg.icon_work, execname))
 
     # Get name of initial file
     if hasattr(cfg, 'inicond_filename'):
@@ -71,7 +74,8 @@ def main(starttime, hstart, hstop, cfg, model_cfg):
             starttime_real.strftime(cfg.meteo_nameformat) + '.nc')
 
     # Write run script (run_icon.job)
-    with open(cfg.icon_runjob) as input_file:
+    icon_runjob = os.path.join(cfg.case_path, cfg.icon_runjob_filename)
+    with open(icon_runjob) as input_file:
         to_write = input_file.read()
     output_file = os.path.join(cfg.icon_work, "run_icon.job")
     with open(output_file, "w") as outf:
