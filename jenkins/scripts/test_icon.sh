e10b102403dc0f5c2fa25c7480b72ae4fc312304
Michael Jähn <mjaehn@ethz.ch>
2023-10-26 13:10:06 +0200

    Fix icon test jenkins files

--- jenkins/scripts/test_icon.sh
+++ jenkins/scripts/test_icon.sh
@@ -10,4 +10,4 @@ function error {
 # Check if script is called correctly
 [[ $(git rev-parse --show-toplevel 2>/dev/null) = $(pwd) ]] || error "$0 not launched from toplevel of repository"
 
-python run_chain.py icon-test 2018-01-01 0 24 -j prepare_data icon -f
+python run_chain.py icon-test -f
