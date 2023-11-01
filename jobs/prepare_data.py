518231b7850ed54f82ffc492b8b474dda5c44f36
Michael Jähn <mjaehn@ethz.ch>
2023-11-01 15:42:53 +0100

    Add prefix to chem file

--- jobs/prepare_data.py
+++ jobs/prepare_data.py
@@ -320,7 +320,7 @@ def main(cfg, model_cfg):
                     chem_file = os.path.join(
                         cfg.icon_input_icbc,
                         time.strftime(cfg.chem_nameformat))
-                    datafile_list_chem.append(chem_file + cfg.chem['suffix'])
+                    datafile_list_chem.append(cfg.chem['prefix'] + chem_file + cfg.chem['suffix'])
                 if meteo_file.endswith('00'):
                     datafile_list.append(meteo_file + cfg.meteo['suffix'])
                 else:
