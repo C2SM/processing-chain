f76cfd74ad14c0892464e26fa611e59a428e09c4
Michael Jähn <mjaehn@ethz.ch>
2023-11-13 12:49:29 +0100

    Use small logo for docs

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_logo = '_static/processing-chain-logo.png'
+html_logo = '_static/processing-chain-logo-small.png'
 html_favicon = '_static/processing-chain-favicon.png'
 html_theme_options = {
     'logo_only': True,
