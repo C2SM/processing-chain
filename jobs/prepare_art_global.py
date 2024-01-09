d2dd23d1d46c062a67f3c1324ae781e9c44835b8
Michael Jähn <mjaehn@ethz.ch>
2024-01-09 11:17:12 +0100

    Make prepare_art_global dependent on previous icon

--- jobs/prepare_art_global.py
+++ jobs/prepare_art_global.py
@@ -12,82 +12,18 @@ from .tools.interpolate_data import create_oh_for_restart, create_oh_for_inicond
 from .tools.fetch_external_data import fetch_era5, fetch_era5_nudging
 
 
-def set_cfg_variables(cfg):
-    if cfg.workflow_name.startswith('cosmo'):
-        cfg.int2lm_root = cfg.chain_root / 'int2lm'
-        cfg.int2lm_input = cfg.int2lm_root / 'input'
-    elif cfg.workflow_name.startswith('icon'):
-        cfg.icon_base = cfg.chain_root / 'icon'
-        cfg.icon_input = cfg.icon_base / 'input'
-        cfg.icon_input_icbc = cfg.icon_input / 'icbc'
-        cfg.icon_work = cfg.icon_base / 'run'
-        cfg.icon_output = cfg.icon_base / 'output'
-        cfg.icon_output_reduced = cfg.icon_base / 'output_reduced'
-        cfg.icon_restart_out = cfg.icon_base / 'restart'
-        cfg.icon_restart_in = cfg.chain_root_prev / 'icon' / 'run'
-        cfg.icon_input_icbc_prev = cfg.chain_root_prev / 'icon' / 'input' / 'icbc'
-
-        cfg.input_files_scratch = {}
-        for dsc, file in cfg.input_files.items():
-            cfg.input_files[dsc] = (p := Path(file))
-            cfg.input_files_scratch[dsc] = cfg.icon_input / p.name
-
-        cfg.create_vars_from_dicts()
-
-        cfg.ini_datetime_string = cfg.startdate.strftime('%Y-%m-%dT%H:00:00Z')
-        cfg.end_datetime_string = cfg.enddate.strftime('%Y-%m-%dT%H:00:00Z')
-
-        if cfg.workflow_name == 'icon-art-oem':
-            cfg.startdate_sim_yyyymmdd_hh = cfg.startdate_sim.strftime(
-                '%Y%m%d_%H')
-
-        if cfg.workflow_name == 'icon-art-global':
-            # Nudge type (global or nothing)
-            cfg.nudge_type = 2 if cfg.era5_global_nudging else 0
-            # Time step for global nudging in seconds
-            cfg.nudging_step_seconds = cfg.nudging_step * 3600
-            # Prescribed initial conditions for CH4, CO and/or OH
-            cfg.iart_init_gas = 4 if cfg.species_inicond else 0
-
-        if cfg.lrestart == '.TRUE.':
-            cfg.restart_filename = 'restart_atm_DOM01.nc'
-            cfg.restart_file = cfg.icon_restart_in / cfg.restart_filename
-            cfg.restart_file_scratch = cfg.icon_work / cfg.restart_filename
-
-        cfg.job_ids['current']['prepare_data'] = []
-
-
 def main(cfg):
     """
-    This function prepares the data for global ICON-ART simulations
-    by downloading necessary meteorological and chemical data,
-    creating the inicond file, and handling global nudging
-    conditions. The workflow includes fetching ERA5 data, processing it, 
-    and creating initial conditions for tracers (such as CH4, CO, and OH).
-
-    The main steps performed by this function include:
-    1. Downloading ERA5 data and creating the inicond file if specified in the
-       configuration.
-    2. Creating initial conditions for tracers (CH4, CO, OH) if needed,
-       including handling restart scenarios.
-    3. Handling global nudging conditions by downloading and processing ERA5 and
-       CAMS data if configured.
-
-    Note
-    ----
-    - The function utilizes external scripts for processing ERA5 data
-      (`icon_era5_inicond.sh`, `icon_era5_nudging.sh`) and CAMS data
-      (`icon_cams_nudging.sh`).
-    - The `tools` module provides various utility functions used in the workflow.
+    Prepare global ICON-ART simulations.
 
     Parameters
     ----------
     cfg : Config
         Object holding all user-configuration parameters as attributes.
     """
-    set_cfg_variables(cfg)
+    prepare_data.set_cfg_variables(cfg)
     launch_time = cfg.init_time_logging("prepare_art_global")
-    logging.info("Preprae ICON-ART for global simulations")
+    logging.info("Prepare ICON-ART for global simulations")
 
     # -- Download ERA5 data and create the inicond file
     if cfg.era5_inicond and cfg.lrestart == '.FALSE.':
