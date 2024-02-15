4dc239f103f83aafbf917b4a6db3fb2a1be361c6
Michael Jähn <mjaehn@ethz.ch>
2024-02-15 11:14:28 +0100

    Build int2lm correctly

--- jenkins/scripts/build_int2lm.sh
+++ jenkins/scripts/build_int2lm.sh
@@ -13,6 +13,13 @@ function error {
 BRANCH=c2sm-features
 GIT_REMOTE=git@github.com:C2SM-RCM/int2lm.git
 
+# Build with gcc on Euler
+if [[ $(hostname) == eu-* ]]; then
+    INT2LM_SPEC="int2lm@${BRANCH}%gcc"
+else
+    INT2LM_SPEC="int2lm@${BRANCH}%nvhpc"
+fi
+
 pushd ext
 # Activate spack
 . spack-c2sm/setup-env.sh
@@ -23,6 +30,6 @@ rm -fr int2lm
 # Clone and build int2lm
 git clone --depth 1 -b ${BRANCH} ${GIT_REMOTE}
     pushd int2lm
-    spack dev-build int2lm @dev-build %nvhpc
+    spack dev-build ${INT2LM_SPEC}
     popd
 popd
