007c8c934011ac788d90176de473cc832e9a138e
Michael Jähn <mjaehn@ethz.ch>
2024-02-19 13:50:09 +0100

    dev-build icontools

--- jenkins/scripts/build_icontools.sh
+++ jenkins/scripts/build_icontools.sh
@@ -10,10 +10,23 @@ function error {
 # Check if script is called correctly
 [[ $(git rev-parse --show-toplevel 2>/dev/null) = $(pwd) ]] || error "$0 not launched from toplevel of repository"
 
+BRANCH=master
+GIT_REMOTE=git@github.com:C2SM/icontools.git
+MODEL=icontools
+COMPILER=gcc
+
 pushd ext
-# Activate spack
-. spack-c2sm/setup-env.sh
 
-# Build icontools
-spack install icontools@c2sm-master%gcc
+# Clone the repo if not already existing
+if [[ ! -d "${MODEL}" ]]; then
+    git clone --depth 1 -b ${BRANCH} ${GIT_REMOTE} ${MODEL}
+fi
 
+pushd ${MODEL}
+
+. ../spack-c2sm/setup-env.sh
+spack dev-build ${MODEL}@${BRANCH}%${COMPILER}
+
+popd
+
+popd
