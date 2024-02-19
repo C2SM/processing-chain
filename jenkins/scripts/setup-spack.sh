4cb23f173464cbd2bf448456b22ade3a7f42f18c
Michael Jähn <mjaehn@ethz.ch>
2024-02-19 10:54:29 +0100

    Use icon spack-tag on daint

--- jenkins/scripts/setup-spack.sh
+++ jenkins/scripts/setup-spack.sh
@@ -13,16 +13,20 @@ function error {
 if [[ $(hostname) == eu-* ]]; then
     source /cluster/apps/local/env2lmod.sh
     module load git/2.31.1
+    SPACK_TAG=main
+else
+    git clone --depth 1 git@github.com:C2SM/icon.git icon-tag
+    SPACK_TAG=`cat config/cscs/SPACK_TAG`
+    rm -fr icon-tag
 fi
 
-TAG=main
 GIT_REMOTE=https://github.com/C2SM/spack-c2sm.git
 
 rm -fr ext/spack-c2sm
 
 pushd ext
 
-git clone --depth 1 --recurse-submodules --shallow-submodules -b ${TAG} ${GIT_REMOTE}
+git clone --depth 1 --recurse-submodules --shallow-submodules -b ${SPACK_TAG} ${GIT_REMOTE}
 
 . spack-c2sm/setup-env.sh
 
